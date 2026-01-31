dacbe7716dd436554298b5775f82acb5977babcd
albertocarm <alberto.carmonabayonas@gmail.com>
2026-01-31 08:44:40 +0000

    Add .Rproj file generation to package creation script
    Includes the RStudio project file in the generated package structure
    so users can open the project directly with double-click in RStudio.

--- utils/crear_libreria.R
+++ utils/crear_libreria.R
@@ -83,6 +83,28 @@ for (archivo in archivos_app) {
   cat("  Copiado:", ruta_rel, "\n")
 }
 
+# --- .Rproj ---
+writeLines(con = file.path(ruta_paquete, paste0(nombre_paquete, ".Rproj")), c(
+  "Version: 1.0",
+  "",
+  "RestoreWorkspace: Default",
+  "SaveWorkspace: Default",
+  "AlwaysSaveHistory: Default",
+  "",
+  "EnableCodeIndexing: Yes",
+  "UseSpacesForTab: Yes",
+  "NumSpacesForTab: 2",
+  "Encoding: UTF-8",
+  "",
+  "RnwWeave: Sweave",
+  "LaTeX: pdfLaTeX",
+  "",
+  "BuildType: Package",
+  "PackageUseDevtools: Yes",
+  "PackageInstallArgs: --no-multiarch --with-keep.source"
+))
+cat("  Creado:", paste0(nombre_paquete, ".Rproj"), "\n")
+
 # --- .Rbuildignore ---
 writeLines(con = file.path(ruta_paquete, ".Rbuildignore"), c(
   "^.*\\.Rproj$",
