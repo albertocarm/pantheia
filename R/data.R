64944e1304aab43b1e8a16aa33e31ebf5ac0f716
albertocarm <alberto.carmonabayonas@gmail.com>
2026-02-01 13:14:24 +0100

    fig_2 fixed

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Datos clínicos del estudio PanTheia
+#'
+#' Dataset conteniendo información clínica y valores SIRI de pacientes.
+#'
+#' @format Un data frame con filas y variables:
+#' \describe{
+#'   \item{logsiri}{Logaritmo del índice SIRI}
+#'   \item{pfs_time1l}{Tiempo de supervivencia libre de progresión}
+#'   ...
+#' }
+#' @source Datos internos del estudio.
+"pantheia_data"
---
+++ R/globals.R
@@ -0,0 +1,26 @@
+utils::globalVariables(c(
+  # Variables generales y de gráficos
+  "x", "y", "grp", "val", "label", "lab", "time", "lower", "upper", "surv",
+  "est", "lo", "hi", "endpoint", "p_val", "panel_group", 
+  
+  # Variables clínicas y demográficas
+  "age", "edad", "sex", "smoking", "alcohol", "smoke_ordinal", "exercise",
+  "familial_cancer", "steroids", 
+  
+  # Variables oncológicas y SIRI
+  "SIRI", "logsiri", "SIRI_cat", "siri_high", 
+  "pfs_time1l", "regimen_cat", "regimen_cat_clean", 
+  "recist_plot", "measurable_disease", 
+  "ecog_cat_3", "ecog_cat_table1",
+  
+  # Síntomas y CACS
+  "asthenia", "anorexia", "weight_loss_bin", "cachexia", 
+  "symp_tumorpain", "CACS_syndrome", "obstructive_jaundice", "vte_basal",
+  
+  # Metástasis y localización
+  "head_pancreas", "body_tail", 
+  "met_liver", "liver_met", "liver_burden",
+  "met_lung", "lung_met", 
+  "met_peritoneum", "peritoneum_met",
+  "met_locoregional_ln"
+))
